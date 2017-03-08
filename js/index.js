35f1f2a8a257e5cc3bec9c8c45afb1c1d61d2e18
ginobilee <ginobilee@outlook.com>
2017-03-08 22:57:29 +0800

    done

--- js/index.js
+++ js/index.js
@@ -15772,7 +15772,7 @@
 	const logs=data.monthlyVariance;
 	const maxTemp=Math.ceil(d3.max(logs,function(d){return d.variance;})),minTemp=Math.floor(d3.min(logs,function(d){return d.variance;}));
 	//console.log(minTemp);
-	const margin={left:80,top:150,right:80,bottom:130},
+	const margin={left:80,top:150,right:30,bottom:130},
 	width=screen.width-100-margin.left-margin.right,
 	height=800-margin.top-margin.bottom;
 	const chart=d3.select(".chart")
@@ -15782,7 +15782,7 @@
 										.attr("transform","translate("+margin.left+","+margin.top+")");
 	//console.log('width:'+width);
 	
-		//add color scale
+	//add color scale
 	const c=d3.hsl(245,45,88),csWidth=520,colorArr=d3.range(minTemp,maxTemp,1),hStart=150,hEnd=-50;
 	const colorScale=d3.scaleLinear()//this is a color scale location 
 												.domain([minTemp,maxTemp])
@@ -15792,7 +15792,7 @@
 												.range([hStart,hEnd]);
 	const colorWidth=csWidth/colorArr.length;
 	const colors=chart.append("g")
-												.attr("transform","translate("+(width/2-100)+","+(height+margin.top-100)+")");										
+												.attr("transform","translate("+(width/2)+","+(height+margin.top-100)+")");										
 	colors.selectAll("rect")
 				.data(colorArr)
 				.enter()
@@ -15806,27 +15806,61 @@
 	const colorAxes=d3.axisBottom(colorScale);	
 	colors.append("g")
 					.attr("transform","translate(0,20)")
-					.call(colorAxes);
+					.call(colorAxes)
+					.append("text")
+						.attr("x",width/2+20)
+						.attr("y",35)
+						.attr("text-anchor","end")
+						.style("font-size","15px")
+						.style("fill","black")
+						.text("This scale is a variance relative to the Jan 1951-Dec 1980 average,which is estimated absolute temperature ℃: 8.66 +/- 0.07.");
 															
 	//construct x scale
 	const xScale=d3.scaleBand()
 											.range([0,width])
 											.domain(logs.map(function(d){return d.year}));
+	const minYear=d3.min(logs,function(d){return d.year}),maxYear=d3.max(logs,function(d){return d.year}),yearArr=d3.range(minYear,maxYear,23);
 	const yearScale=d3.axisBottom(xScale)
-												.ticks(10);
+												.tickSizeOuter(0)
+												.tickValues([...yearArr,2015]);
 	chart.append("g")
 					.attr("transform","translate(0,"+height+")")
 					.call(yearScale);
-	//console.log('2015:'+xScale(2015));
 	const fragWidth=xScale.bandwidth();	
 	//console.log('fragWidth:'+fragWidth);
+	
 	//construct y scale
 	const yScale=d3.scaleBand()
 										.rangeRound([0,height])
 										.domain(logs.map(function(d){return d.month}));
+	const months=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+	const monthScale=d3.scaleBand()
+												.rangeRound([0,height])
+												.domain(months);
+	const yAxes=d3.axisLeft(monthScale)
+												.tickSizeOuter(0);
+	chart.append("g")
+					.attr("transform","translate(0,0)")
+					.attr("class","y axis")
+					.call(yAxes)
+				.append("text")
+					.attr("transform","rotate(-90)")
+					.attr("x",-220)
+					.attr("y",-45)
+					.style("font-size","25px")
+					.style("fill","black")
+					.text("Months");
+														
 	//console.log(yScale(12));
 	const fragHeight=yScale.bandwidth();
 	
+	//initiate tips
+	const valFormat=d3.format(".2f");
+	const tip = d3.tip()
+									.attr('class', 'd3-tip')
+									.offset([-10, 0])
+									.html(function(d) { return "<strong>Year:"+d.year+"<br />Month:"+months[d.month-1]+"<br /></strong> <span style='color:red'>" + valFormat(d.variance+8.66) + "℃</span>"; });
+	chart.call(tip);
 	const rects=chart.append("g")
 											.selectAll("rect")
 												.data(logs)
@@ -15837,8 +15871,7 @@
 												.attr("height",fragHeight)
 												.attr("x",function(d){return xScale(d.year)})
 												.attr("y",function(d){return yScale(d.month)})
-												.style("fill",function(d){let h=hScale(d.variance);return d3.hsl(h,1,0.45);});		
-
-	//add color scale
-	//chart.append("
+												.style("fill",function(d){let h=hScale(d.variance);return d3.hsl(h,1,0.45);})
+												.on('mouseover', tip.show)
+      									.on('mouseout', tip.hide);		
 }
